a7a7307a6ee7e2895a26cb1c471d6f15ed7da510
Joan Pastor <joapaspe@gmail.com>
2013-09-17 18:30:51 +0200

    Responsive menu closes on click

--- js/joan.js
+++ js/joan.js
@@ -1,7 +1,13 @@
 				$(document).ready(function() {
 					$('.carousel').carousel();
 
-					//$('.navbar').scrollspy({data-offset:'10'});
+
+					// Responsive nav-bar
+					$(".navbar li a").click(function(event) {
+       				 // check if window is small enough so dropdown is created
+    				$(".navbar-collapse").removeClass("in").addClass("collapse");
+    				});
+    				//$('.navbar').scrollspy({data-offset:'10'});
 					//$('.navbar').scrollspy('refresh')
 					$("#nav-home").toggleClass("active", true);
 
